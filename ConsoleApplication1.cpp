071ac7a3a9cc6d72c901bc804fd80841cddc2957
oluwadec <oluwadeclanrice41@gmail.com>
2026-06-22 17:42:30 +0100

    Added my second challenge

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int num1;
-	int num2;
-	int num3;
-	cin >> num1;
-	cin >> num2;
-	cin >> num3;
-	int sum = num1 + num2 + num3;
-	cout << " The total sum is : " << sum << endl;
+	double length;
+	double width;
+	double area;
+	cin >> length;
+	cin >> width;
+	double area = length * width;
+	cout << " The area of the rectangle is :" << area << endl;
 	return 0;
 }
